bf4bf683637b1ef98f6779d68a54fae97c3b1a60
Neil Dahiya <neildahiya1@gmail.com>
2020-05-07 03:12:20 +0530

    sign in sign up using email added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 export default class SignIn extends Component {
@@ -12,8 +12,19 @@ export default class SignIn extends Component {
       password: "",
     };
   }
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        name: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,31 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyCtg4n7U2zDFdppuO3k0R0849tD2UW4AOQ",
-  authDomain: "crwn-db-36555.firebaseapp.com",
-  databaseURL: "https://crwn-db-36555.firebaseio.com",
-  projectId: "crwn-db-36555",
-  storageBucket: "crwn-db-36555.appspot.com",
-  messagingSenderId: "557595421370",
-  appId: "1:557595421370:web:9f76dbf9ecb3ee17157083",
-  measurementId: "G-C7ZPB1TLCC",
+const config = {};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user ", error.message);
+    }
+  }
+  return userRef;
+  //   console.log(snapShot);
 };
 
 firebase.initializeApp(config);
