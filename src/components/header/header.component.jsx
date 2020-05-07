7cebe5fe3fe12b488fcf66622cddb58269f81af6
Neil Dahiya <neildahiya1@gmail.com>
2020-05-07 14:49:05 +0530

    basic redux setup in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,30 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+
 import "./header.styles.scss";
-export default function Header({ currentUser }) {
-  return (
-    <div className="header">
-      <Link to="/" className="logo-container">
-        <Logo className="logo"></Logo>
+
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link to="/" className="logo-container">
+      <Logo className="logo"></Logo>
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
       </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          SHOP
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className="option" to="/signIn">
+          Sign In
         </Link>
-        <Link className="option" to="/shop">
-          CONTACT
-        </Link>
-        {currentUser ? (
-          <div className="option" onClick={() => auth.signOut()}>
-            Sign Out
-          </div>
-        ) : (
-          <Link className="option" to="/signIn">
-            Sign In
-          </Link>
-        )}
-      </div>
+      )}
     </div>
-  );
-}
+  </div>
+);
+
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
