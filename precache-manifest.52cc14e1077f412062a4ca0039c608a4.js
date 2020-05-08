2155954ec58f3654da50de596b7e836644b17a1f
Neil Dahiya <neildahiya1@gmail.com>
2020-05-08 09:39:21 +0530

    Updates

---
+++ precache-manifest.52cc14e1077f412062a4ca0039c608a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438cf31d77521bb2668f6df57bd600eb",
+    "url": "/eCommerce-React/index.html"
+  },
+  {
+    "revision": "da1f0566fe020508b806",
+    "url": "/eCommerce-React/static/css/main.72b1c33e.chunk.css"
+  },
+  {
+    "revision": "328df03c856360d2bdfc",
+    "url": "/eCommerce-React/static/js/2.dcc3b2ee.chunk.js"
+  },
+  {
+    "revision": "bd6b762390574ac8c479bbcc74f7e273",
+    "url": "/eCommerce-React/static/js/2.dcc3b2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1f0566fe020508b806",
+    "url": "/eCommerce-React/static/js/main.8a8c5801.chunk.js"
+  },
+  {
+    "revision": "0b3b59a3d76d8f8df58e",
+    "url": "/eCommerce-React/static/js/runtime-main.8a2b342e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/eCommerce-React/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/eCommerce-React/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d604044cb971599f4b7fc0a6caf7d4f5.js"
+  "/eCommerce-React/precache-manifest.52cc14e1077f412062a4ca0039c608a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eCommerce-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
