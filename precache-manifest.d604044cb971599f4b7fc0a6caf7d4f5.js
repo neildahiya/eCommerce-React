329ec68052af9a9e89f05f44d316b609416808a9
Neil Dahiya <neildahiya1@gmail.com>
2020-05-08 09:32:16 +0530

    Updates

---
+++ precache-manifest.d604044cb971599f4b7fc0a6caf7d4f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fd426f04ea37add359771211c61f25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da1f0566fe020508b806",
+    "url": "/static/css/main.72b1c33e.chunk.css"
+  },
+  {
+    "revision": "328df03c856360d2bdfc",
+    "url": "/static/js/2.dcc3b2ee.chunk.js"
+  },
+  {
+    "revision": "bd6b762390574ac8c479bbcc74f7e273",
+    "url": "/static/js/2.dcc3b2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1f0566fe020508b806",
+    "url": "/static/js/main.8a8c5801.chunk.js"
+  },
+  {
+    "revision": "ddd11a326e6cac45944b",
+    "url": "/static/js/runtime-main.a698031a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
